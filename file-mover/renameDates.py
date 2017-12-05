7f0696b24a7b76ba4831f79415bd8df54a2e77f6
arousselDDA <alex@deepdiveanalytics.com.au>
2017-12-06 08:26:20 +1100

    added a renameDates python file and a test subdirectory

--- file-mover/renameDates.py
+++ file-mover/renameDates.py
@@ -2,14 +2,32 @@
  # renameDates.py - Renames filenames with American MM-DD-YYYY date format
  # to European DD-MM-YYYY.
 
-import shutils, os, re
+import shutil, os, re
 
 #Create a regex that matches files with the American date format.
-datePattern = re.compile(r"""^(.*?) # all text before the date
+datePattern = re.compile(r"""^(.*?)    # all text before the date
        ((0|1)?\d)-                     # one or two digits for the month
        ((0|1|2|3)?\d)-                 # one or two digits for the day
        ((19|20)\d\d)                   # four digits for the year
        (.*?)$                          # all text after the date
-❸     """, re.VERBOSE)
+      """, re.VERBOSE)
 
-# TODO: Loop over the files in the working directory.
+for amerFilename in os.listdir('C:\\Users/AlexDeepDive/Documents/DEV/python-sys-jobs/file-mover/test'):
+    mo = datePattern.search(amerFilename)
+
+    if mo == None:
+        continue
+
+    beforePart = mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(4)
+    yearPart = mo.group(6)
+    afterPart = mo.group(8)
+
+    euroFilename = beforePart + dayPart+ '-' + monthPart + '-' + yearPart + afterPart
+    absWorkingDir = os.path.abspath('./test')
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
+
+    print('renaming "%s" to "%s"...' % (amerFilename, euroFilename))
+    os.rename(amerFilename, euroFilename)
